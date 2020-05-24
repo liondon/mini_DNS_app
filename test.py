8315e04b877955682a31999abb1debadc5f47269
liondon <liondonaugust810810@gmail.com>
2020-05-23 22:40:04 -0400

    chore: organize files and create README

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
 from flask import jsonify
-US_hostname = "127.0.0.1"
+US_ip = "127.0.0.1"
 US_port = 8080
 FS_ip = "127.0.0.1"
 FS_hostname = "fibonacci.com"
@@ -10,12 +10,12 @@ AS_ip = "127.0.0.1"
 AS_port = "53533"
 
 ## US: correct request: response the fibonacci number
-url = "http://{}:{}/fibonacci?hostname={}&fs_port={}&number={}&as_ip={}&as_port={}".format(US_hostname, US_port, FS_hostname, FS_port, number, AS_ip, AS_port)
+url = "http://{}:{}/fibonacci?hostname={}&fs_port={}&number={}&as_ip={}&as_port={}".format(US_ip, US_port, FS_hostname, FS_port, number, AS_ip, AS_port)
 response = requests.get(url, timeout=2.50)
 print(response.status_code, response.text)
 
 ## US: bad request: response 404
-url = "http://{}:{}/fibonacci?hostname={}&fs_port={}&number={}&as_ip={}".format(US_hostname, US_port, FS_hostname, FS_port, number, AS_ip, AS_port)
+url = "http://{}:{}/fibonacci?hostname={}&fs_port={}&number={}&as_ip={}".format(US_ip, US_port, FS_hostname, FS_port, number, AS_ip, AS_port)
 response = requests.get(url, timeout=2.50)
 print(response.status_code, response.text)
 
